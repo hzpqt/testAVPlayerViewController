07b09c0ec82b9bc6d01d14d3329b6aacdeabcf61
peng_qitao <peng_qitao@sun-zhongqiangdeMac-mini.local>
2016-07-06 15:08:02 +0800

    first commit

---
+++ testAVPlayerViewController/AVPlayerAudioViewController.h
@@ -0,0 +1,13 @@
+//
+//  AVPlayerAudioViewController.h
+//  testAVPlayerViewController
+//
+//  Created by peng_qitao on 16/7/6.
+//  Copyright © 2016年 peng_qitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AVPlayerAudioViewController : UIViewController
+
+@end
---
+++ testAVPlayerViewController/LCAVPlayerViewController.m
@@ -0,0 +1,13 @@
+//
+//  LCAVPlayerViewController.m
+//  testAVPlayerViewController
+//
+//  Created by peng_qitao on 16/6/20.
+//  Copyright © 2016年 peng_qitao. All rights reserved.
+//
+
+#import "LCAVPlayerViewController.h"
+
+@implementation LCAVPlayerViewController
+
+@end
